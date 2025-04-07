6078c1af2e049a6048b5703f8739855c2f624db1
Salomon Avila <104867468+SalomonAvila@users.noreply.github.com>
2025-04-07 15:08:51 -0500

    Update 2023-30-2.cpp

--- parciales/Segundo parcial/2023-30-2.cpp
+++ parciales/Segundo parcial/2023-30-2.cpp
@@ -2,47 +2,54 @@
 
 using namespace std;
 
-int cucunuba(int lanzamientos[], int cantidadLanzamientos, int agujeros[], int numeroAgujeros){
+void llenar(int agujero[], int posiciones);
+int calcularPuntos(int lanzamientos[], int numeroLanzamientos, int agujero[], int numeroAgujeros);
+
+
+int main(){
     
+    int numeroAgujeros, numeroLanzamientosUno, numeroLanzamientosDos;
+    cin>>numeroAgujeros;
+    cin>>numeroLanzamientosUno;
+    cin>>numeroLanzamientosDos;
+    int agujero[numeroAgujeros];
+    int lanzamientosUno[numeroLanzamientosUno];
+    int lanzamientosDos[numeroLanzamientosDos];
+    
+    llenar(agujero, numeroAgujeros);
+    llenar(lanzamientosUno, numeroLanzamientosUno);
+    llenar(lanzamientosDos, numeroLanzamientosDos);
+    
+    int puntosUno = calcularPuntos(lanzamientosUno, numeroLanzamientosUno, agujero, numeroAgujeros);
+    int puntosDos = calcularPuntos(lanzamientosDos, numeroLanzamientosDos, agujero, numeroAgujeros);
+    
+    if(puntosUno == puntosDos){
+        cout<<"Empate"<<endl;
+    }else{
+        if(puntosUno>puntosDos){
+            cout<<"Ganador: Jugador uno"<<endl;
+        }else{
+            cout<<"Ganador: Jugador dos"<<endl;
+        }
+    }
+}
+
+
+void llenar(int agujero[], int posiciones){
+    for(int i = 0; i<posiciones; i++){
+        int valor;
+        cin>>valor;
+        agujero[i] = valor;
+        //cin>>agujero[i];
+    }
+}
+
+int calcularPuntos(int lanzamientos[], int numeroLanzamientos, int agujero[], int numeroAgujeros){
     int puntos = 0;
-    for(int i = 0; i<cantidadLanzamientos; i++){
-        if(lanzamientos[i] > -1){
-            puntos +=  agujeros[lanzamientos[i]];
+    for(int i = 0; i<lanzamientos; i++){
+        if(lanzamientos[i] != -1){
+            puntos += agujero[lanzamientos[i]]
         }
     }
     return puntos;
-    
-}
-
-int main(){
-    int numeroAgujeros;
-    cin>>numeroAgujeros;
-    int agujeros[numeroAgujeros];
-    int cantidadLanzamientosUno;
-    cin>>cantidadLanzamientosUno;
-    int lanzamientosUno[cantidadLanzamientosUno];
-    int cantidadLanzamientosDos;
-    cin>>cantidadLanzamientosDos;
-    int lanzamientosDos[cantidadLanzamientosDos];
-
-    for(int i = 0; i<numeroAgujeros; i++){
-        cin>>agujeros[i];
-    }
-    for(int i = 0; i<cantidadLanzamientosUno; i++){
-        cin>>lanzamientosUno[i];
-    }
-    for(int i = 0; i<cantidadLanzamientosDos; i++){
-        cin>>lanzamientosDos[i];
-    }
-    
-    int puntosUno = cucunuba(lanzamientosUno, cantidadLanzamientosUno, agujeros, numeroAgujeros);
-    int puntosDos = cucunuba(lanzamientosDos, cantidadLanzamientosDos, agujeros, numeroAgujeros);
-    
-    if(puntosDos == puntosUno){
-        cout<<"Empate"<<endl;
-    }else if(puntosUno > puntosDos){
-        cout<<"Gana el jugador Uno con "<<puntosUno<<" puntos"<<endl;
-    }else{
-        cout<<"Gana el jugador dos con "<<puntosDos<<" puntos"<<endl;
-    }
 }
